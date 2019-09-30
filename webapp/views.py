b5bf0566db85386f22b32bda53cddeed705ab8b2
tyzo4ka <tyzo4ka@gmail.com>
2019-09-30 13:38:42 +0600

    added all files from last lesson:

---
+++ webapp/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import TemplateView
+
+from webapp.forms import ArticleForm, CommentForm
+from webapp.models import Article, Comment
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+
+
+class ArticleView(TemplateView):
+    template_name = 'article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_pk = kwargs.get('pk')
+        context['article'] = get_object_or_404(Article, pk=article_pk)
+        return context
+
+
+class ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                category=form.cleaned_data['category']
+            )
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'create.html', context={'form': form})
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        form = ArticleForm(data={
+            'title': article.title,
+            'author': article.author,
+            'text': article.text,
+            'category': article.category_id
+        })
+        return render(request, 'update.html', context={'form': form, 'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data['title']
+            article.author = form.cleaned_data['author']
+            article.text = form.cleaned_data['text']
+            article.category = form.cleaned_data['category']
+            article.save()
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'update.html', context={'form': form, 'article': article})
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        return render(request, 'delete.html', context={'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        article.delete()
+        return redirect('index')
+
+
+class CommentView(TemplateView):
+    template_name = 'comment/comments_index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comment_pk = kwargs.get('pk')
+        context['comments'] = Comment.objects.all()
+        return context
+
+
+class CommentCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = CommentForm()
+        return render(request, 'comment/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment = Comment.objects.create(
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                article=form.cleaned_data['article']
+            )
+            return redirect('comments')
+        else:
+            return render(request, 'comment/create.html', context={'form': form})
